18322cd3ae680f8ce9549c20f68d8744ff785acc
Cameron Ross <elpenguino@gmail.com>
2014-03-12 18:12:20 -0300

    additional work on parser, should now parse WAE mostly correctly

--- core.clj
+++ core.clj
@@ -8,54 +8,51 @@
 	"parser for ae language, output is intended to be piped into interpreter.
 
 	Will recurse using grammar:
-	<AE> ::= <num> 
-	 | {+ <AE> <AE>} 
-	 | {- <AE> <AE>}
+	<WAE> ::= <num> 
+	 | {+ <WAE> <WAE>} 
+	 | {- <WAE> <WAE>}
+	 | {* <WAE> <WAE>}
+	 | {/ <WAE> <WAE>}
+	 | {with {{<id> <WAE>} ...} <WAE>}
+	 | <id>
 
-	Base case is just a number. Will recurse if first symbol is an operator (+/-)
+	Base case is just a number. Will recurse if first symbol is an operator (+, -, *, /, with)
 	"
 	[ae]
 	;check type
 	(if-not (= (type ae) clojure.lang.PersistentList ) ;check if argument passed in is a list
-		(if (number? ae) ;check if its a number
-			(list "num" ae)
-
-			(println "Error,", ae, "is not a valid number or symbol") ;will happen is not a list or a number
-		) ;end of if its a number
+		(cond 
+			(number? ae) (list "num" ae) ; if number, prepend "num"
+			:else (list "id" ae) ;otherwise it's an identifier
+		) ;end of if its a number or identifier
 
 		;else if it is a list this next part will exectute 
 		(let [leftmost (nth ae 0)]
 		(let [loperand (nth ae 1 "Didn't find loperand")] ;might not be there
 		(let [roperand (nth ae 2 "Didn't find roperand")] ;might not be there
-
-			(if (= "+" (str leftmost)) ;is leftmost a plus sign?
-				;return a length 3 list. The second two entries are recursive 
-				;calls on aprs with the left and right operands
-				(list "add" (pars loperand) (pars roperand)
-				) ;from print inside plus check
-
-				(if (= "-" (str leftmost)) ;else is it a minus sign?
-					;return a length 3 list. The second two entries are recursive 
-					;calls on aprs with the left and right operands
-					(list "sub" (pars loperand) (pars roperand) )
-
-					(println "Error,", leftmost, "is not a valid operator") ;reaches this line if it is not a subtraction or addition
-
-				) ;sub (if that is nested)
-
-			) ;plus (bigger if)
-
+		(cond 
+			(= "+" (str leftmost)) (list "add" (pars loperand) (pars roperand)) ;if +, return a length 3 list with "add" and the operands
+			(= "-" (str leftmost)) (list "sub" (pars loperand) (pars roperand)) ;if -, return a length 3 list with "sub" and the operands
+			(= "*" (str leftmost)) (list "mul" (pars loperand) (pars roperand)) ;if *, return a length 3 list with "mul" and the operands
+			(= "/" (str leftmost)) (list "div" (pars loperand) (pars roperand)) ;if /, return a length 3 list with "div" and the operands
+			(= "with" (str leftmost)) (list "with" (pars loperand) (pars roperand)) ;with works pretty much the same way in the parser
+			:else (map pars ae) ;parsing the first arg for with here
+		) ;cond
 		) ;let roperand
 		) ;let loperand
 		) ;let leftmost
-		;end of else (from if this is a list)
-
 	) ;check type
 
 );pars
 
+
+(defn interp
+	[ae]
+	(println ae)
+);interp
+
 (defn -main
   	"calls the pars and interp with given ae"
 	[ae]
-	(println (pars ae)) ;will soon pipe this into interp (which doesn't exist yet)
+	(interp (pars ae)) ;will soon pipe this into interp (which doesn't exist yet)
  ) ;-main
