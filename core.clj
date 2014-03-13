2d352249aac2bf4fc7b8548a97d61cdade689f08
Cameron Ross <elpenguino@gmail.com>
2014-03-12 21:38:44 -0300

    prepare to implement with in interpreter

--- core.clj
+++ core.clj
@@ -4,10 +4,12 @@
 ;Initial parser created by Federico Mora, 2014
 ;Modified by Cameron Ross, 2014
 
+(declare pwith)
+(declare iwith)
 (defn pars
-	"parser for ae language, output is intended to be piped into interpreter.
+	"Parser for WAE language, output is intended to be piped into WAE interpreter.
 
-	Will recurse using grammar:
+	Will recurse using the following grammar:
 	<WAE> ::= <num> 
 	 | {+ <WAE> <WAE>} 
 	 | {- <WAE> <WAE>}
@@ -16,43 +18,79 @@
 	 | {with {{<id> <WAE>} ...} <WAE>}
 	 | <id>
 
-	Base case is just a number. Will recurse if first symbol is an operator (+, -, *, /, with)
+	Base case is just a number or an identifier. Will recurse if first symbol is an operator (+, -, *, /, with)
 	"
-	[ae]
+	[wae]
 	;check type
-	(if-not (= (type ae) clojure.lang.PersistentList ) ;check if argument passed in is a list
-		(cond 
-			(number? ae) (list "num" ae) ; if number, prepend "num"
-			:else (list "id" ae) ;otherwise it's an identifier
-		) ;end of if its a number or identifier
+	(cond
+		(not= (type wae) clojure.lang.PersistentList ) ;check if argument passed in is a list
+			(if (number? wae)
+				(list "num" wae) ; if number, prepend "num"
+				(list "id" wae)  ; otherwise it's an identifier
+			) ;end of if its a number or identifier
 
-		;else if it is a list this next part will exectute 
-		(let [leftmost (nth ae 0)]
-		(let [loperand (nth ae 1 "Didn't find loperand")] ;might not be there
-		(let [roperand (nth ae 2 "Didn't find roperand")] ;might not be there
-		(cond 
-			(= "+" (str leftmost)) (list "add" (pars loperand) (pars roperand)) ;if +, return a length 3 list with "add" and the operands
-			(= "-" (str leftmost)) (list "sub" (pars loperand) (pars roperand)) ;if -, return a length 3 list with "sub" and the operands
-			(= "*" (str leftmost)) (list "mul" (pars loperand) (pars roperand)) ;if *, return a length 3 list with "mul" and the operands
-			(= "/" (str leftmost)) (list "div" (pars loperand) (pars roperand)) ;if /, return a length 3 list with "div" and the operands
-			(= "with" (str leftmost)) (list "with" (pars loperand) (pars roperand)) ;with works pretty much the same way in the parser
-			:else (map pars ae) ;parsing the first arg for with here
-		) ;cond
-		) ;let roperand
-		) ;let loperand
-		) ;let leftmost
+		:else ;looks like we got a list here
+			(let [leftmost (nth wae 0)]
+			(let [loperand (nth wae 1 "Didn't find loperand")] ;might not be there
+			(let [roperand (nth wae 2 "Didn't find roperand")] ;might not be there
+			(cond 
+				(= "+" (str leftmost))    (list "add"  (pars loperand) (pars roperand)) ;if +, return a length 3 list with "add" and the operands
+				(= "-" (str leftmost))    (list "sub"  (pars loperand) (pars roperand)) ;if -, return a length 3 list with "sub" and the operands
+				(= "*" (str leftmost))    (list "mul"  (pars loperand) (pars roperand)) ;if *, return a length 3 list with "mul" and the operands
+				(= "/" (str leftmost))    (list "div"  (pars loperand) (pars roperand)) ;if /, return a length 3 list with "div" and the operands
+				(= "with" (str leftmost)) (pwith loperand roperand) ;with works pretty much the same way in the parser, but we need some extra error checking
+				
+				:else (map pars wae) ;parsing the first arg for with here
+			) ;cond
+			) ;let roperand
+			) ;let loperand
+			) ;let leftmost
 	) ;check type
-
 );pars
 
+(defn pwith
+	"Handles with statements in the parser.
+	Split into its own function in order to keep the parser tidy.
+	Will throw an exception if duplicate identifiers are detected in the statement.
+	"
+	[ids wae]
+	(do (if (not= (distinct (map first ids)) (map first ids)) (throw (Exception. "Duplicate identifiers"))))
+	(list "with" (pars ids) (pars wae))
+	
+);with
+
+(defn iwith
+	"Handles with statements in the interpreter.
+	Like with in the parser, this has been split into its own function to keep things tidy.
+	"
+	[ids evals]
+	(println ids evals)
+)
 
 (defn interp
-	[ae]
-	(println ae)
+	"Interpreter for the WAE language. Will hopefully output a number corresponding to the parsed input.
+	Intended to be used with the WAE parser.
+	"
+	[wae]
+	(cond
+		(= (type       (first wae))  clojure.lang.PersistentList) (interp (first wae))
+		(= "num"  (str (first wae))) (second wae)                                   ; just a number.
+		(= "add"  (str (first wae))) (+ (interp (second wae)) (interp (nth wae 2))) ; adding two things together
+		(= "sub"  (str (first wae))) (- (interp (second wae)) (interp (nth wae 2))) ; handling subtraction
+		(= "mul"  (str (first wae))) (* (interp (second wae)) (interp (nth wae 2))) ; multiplication!
+		(= "div"  (str (first wae))) (/ (interp (second wae)) (interp (nth wae 2))) ; division probably
+		(= "with" (str (first wae))) (iwith (second wae) (nth wae 2))               ; with statements!
+		:else (throw (Exception. "Unknown operator reached!"))                      ; what is this mess?! don't give me input I don't understand!
+	) ;cond
 );interp
 
-(defn -main
-  	"calls the pars and interp with given ae"
-	[ae]
-	(interp (pars ae)) ;will soon pipe this into interp (which doesn't exist yet)
- ) ;-main
+(defn run
+  	"calls the pars and interp with given wae"
+	[wae]
+	(interp (pars wae))
+ ) ;run
+ 
+ (defn parsestr
+	[str]
+	(run (clojure.string/replace str #"\{|\}" {"{" "(" "}" ")"}))
+ ) ;parsestr
--- test.clj
+++ test.clj
@@ -1,8 +1,21 @@
 (load "core")
 (use 'parse-interp.core)
-(-main '(4))
-(-main '(+ 1 2))
-(-main '(+ (- 4 4) 5))
-(-main '(with ((x (+ 5 5))) (with (y (- x 3)) (+ y y))))
-(-main '(with ((x (+ 5 5)) (y (+ 3 3))) (+ x y)))
-(-main '(with ((x (+ 5 5)) (x (+ 3 3))) (+ x x)))
+
+(defn run-test
+	[program result testid]
+	(if (not= (run program) result) (println "Test" (str testid) "Failed"))
+); run-test
+(defn run-failure-test
+	[program testid]
+	(try (run program) (println "Test" (str testid) "Failed") (catch Exception e))
+); run-failure-test
+(run-test '(4)             4 1)
+(run-test '(+ 1 2)         3 2)
+(run-test '(+ (- 4 4) 5)   5 3)
+(run-test '(+ (- 4 4) 50) 50 4)
+(run-test '(with ((x (+ 5 5))) (with ((y (- x 3))) (+ x y))) 17 5)
+(run-test '(with ((x (+ 5 5))) (with ((x (- x 3))) (+ x x))) 14 6)
+(run-test '(with ((x (+ 5 5)) (y (+ 3 3))) (+ x y))          16 7)
+;(parsestr "{with {{x {+ 5 5}} {y {+ 3 3}}} {+ x y}}")
+;(try (run '(with ((x (+ 5 5)) (x (+ 3 3))) (+ x x))) (println "Test 8 Failed") (catch Exception e))
+(run-failure-test '(with ((x (+ 5 5)) (x (+ 3 3))) (+ x x)) 8)
